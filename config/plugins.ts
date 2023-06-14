eb44ee1a29523e54b7e99f5965aa3eaa00dd886b
Sohel Shekh <contact.sohelshekh@gmail.com>
2023-06-14 17:22:58 +0530

    Intitial Git Upload

--- config/plugins.ts
+++ config/plugins.ts
@@ -14,5 +14,22 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  // ...
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "no-reply@soulspace.in",
+        defaultReplyTo: "no-reply@soulspace.in",
+      },
+    },
+  },
 });
