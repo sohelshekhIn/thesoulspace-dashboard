549bd583e3a11544b1713fe3d681a053107e3ebb
Sohel Shekh <contact.sohelshekh@gmail.com>
2023-09-23 18:21:52 +0530

    Size and Order PG Webhook Optimised

---
+++ src/api/painting-sizes-available/services/painting-sizes-available.ts
@@ -0,0 +1,7 @@
+/**
+ * painting-sizes-available service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::painting-sizes-available.painting-sizes-available');
